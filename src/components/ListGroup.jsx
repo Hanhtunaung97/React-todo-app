8c84a9515594dacd0afdc3f0dd1e69398ee4881c
Hanhtunaung97 <littlehanhtunaung97>
2024-03-25 19:25:27 +0630

    all done btn updated on emptylist

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -2,19 +2,41 @@ import React from "react";
 import EmptyList from "./EmptyList";
 import List from "./List";
 
-const ListGroup = ({  tasks,doneTask,deleteTask,updateTask,doneAllTask }) => {
-  const handleAllDoneBtn=() => {
+const ListGroup = ({
+  tasks,
+  doneTask,
+  deleteTask,
+  updateTask,
+  doneAllTask,
+}) => {
+  const handleAllDoneBtn = () => {
     doneAllTask();
-  }
+  };
   return (
     <div>
-      {tasks.length===0 && <EmptyList/>}
+      {tasks.length === 0 && <EmptyList />}
       {tasks.map(({ id, job, isDone }) => (
-        <List doneAllTask={doneAllTask} updateTask={updateTask} deleteTask={deleteTask}  doneTask={doneTask} key={id} id={id} job={job} isDone={isDone} />
+        <List
+          doneAllTask={doneAllTask}
+          updateTask={updateTask}
+          deleteTask={deleteTask}
+          doneTask={doneTask}
+          key={id}
+          id={id}
+          job={job}
+          isDone={isDone}
+        />
       ))}
-      <div className="flex justify-end">
-        <button onClick={handleAllDoneBtn} className=" bg-neutral-100 px-3 py-1 border border-neutral-700">All Done</button>
-      </div>
+      {tasks.length !== 0 && (
+        <div className="flex justify-end">
+          <button
+            onClick={handleAllDoneBtn}
+            className=" bg-neutral-100 px-3 py-1 border border-neutral-700"
+          >
+            All Done
+          </button>
+        </div>
+      )}
     </div>
   );
 };
